69572106e837e1d81195faa02c57d43a2141d76c
Ramom <ramomcartillo@gmail.com>
2020-03-31 16:47:03 -0300

    criacao das endpoints

---
+++ pontos_turisticos/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls import include
+from rest_framework import routers
+from core.api.viewsets import PontoTuristicoViewSet
+from atracoes.api.viewsets import AtracaoViewSet
+
+
+router = routers.DefaultRouter()
+router.register(r'pontoturistico', PontoTuristicoViewSet)
+router.register(r'atracoes', AtracaoViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('admin/', admin.site.urls),
+]
